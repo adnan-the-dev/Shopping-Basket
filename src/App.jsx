ae5259c7b91f2773862bf33db4daf3f1becf1cd2
adnan <azizart320480@gmail.com>
2024-02-22 01:59:47 +0500

    some change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Home from "./components/home/Home";
-import Testing from "./components/test/Testing";
 
 function App() {
   return (
     <>
       <Home />
-      {/* <Testing/> */}
     </>
   );
 }
